25073dfbead259b6bcae661f629251803ea19453
Pedro de Medeiros <pedro.medeiros@gmail.com>
2024-01-04 09:06:52 -0300

    new function restore_range2

--- restore.tcl
+++ restore.tcl
@@ -5,8 +5,15 @@ proc restore_addr {addr} {
     debug set_watchpoint write_mem $addr {} "poke $addr $oldvalue"
 }
 
+
 proc restore_range {addr size} {
     set mem [debug read_block memory $addr $size]
     set addr2 [expr {$addr + $size}]
+    debug set_watchpoint write_mem "$addr $addr2" {} "poke \$::wp_last_address \[scan \[string range \"$mem\" \[expr \$::wp_last_address - $addr\] 1\] %c\]"
+}
+
+
+proc restore_range2 {addr addr2} {
+    set mem [debug read_block memory $addr [expr {$addr2 - $addr}]]
     debug set_watchpoint write_mem "$addr $addr2" {} "poke \$::wp_last_address \[scan \[string range \"$mem\" \[expr \$::wp_last_address - $addr\] 1\] %c\]"
 }
