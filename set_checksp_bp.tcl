77bb244f3cebdfe1bdcd1811cd688b03b26a89b7
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-11-26 18:10:27 -0300

    Add -symbol option to set_checksp_bp.tcl

--- set_checksp_bp.tcl
+++ set_checksp_bp.tcl
@@ -1,4 +1,16 @@
-proc set_checksp_bp {addr} {
+proc set_checksp_bp {args} {
+  if {[llength $args] == 1} {
+    set type {-addr}
+    lassign $args addr
+  } elseif {[llength $args] == 2} {
+    lassign $args type addr
+  } else {
+    error {wrong # args: should be "set_checksp_bp ?-addr|-symbol? address_or_symbol}
+  }
+  if {[lsearch -exact [list "-addr" "-symbol"] $type] == -1} {
+    error "1st parameter should be -addr or -symbol"
+  }
+  if {$type eq "-symbol"} { set addr [expr $::sym($addr)] }
   # before call set ::addr_sp to [reg SP]
   debug breakpoint create -address "0x[format %x [expr $addr]]" -command "set ::addr_sp \[reg SP\]; puts \"::addr_sp = \$::addr_sp\""
   # after call stops if ::addr_sp not equals [reg SP]
